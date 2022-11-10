fb3f3bbd24c0f026e5aa8c5f937cdfee487472b8
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2022-11-10 11:07:57 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
+/*from  w w w  .j  a v a2s.c  o m*/
+import javax.swing.JPanel;
+
+public class Main extends javax.swing.JFrame {
+  Image backgroundImage = Toolkit.getDefaultToolkit().getImage("C:/Users/Aluno/workspace/C/src/estrela.jpeg");
+
+  public Main() throws IOException {
+    this.setContentPane(new JPanel() {
+      @Override
+      public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, null);
+      }
+    });
+
+    pack();
+    setVisible(true);
+  }
+
+  public static void main(String[] args) throws Exception {
+	  
+    
+              new GifAnimation();
+
+                new Main();
+    
+     
+  }
+}
